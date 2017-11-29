3d3ee0402c6d1ea30e46362edda102749ac58708
Mathias Bellerby Sylvester Nielsen <math130u@edu.eal.dk>
2017-11-29 14:18:03 +0100

    Both desktop and web applications can access the service

--- Tournament Web Interface/Tournament Web Interface/Controllers/HomeController.cs
+++ Tournament Web Interface/Tournament Web Interface/Controllers/HomeController.cs
@@ -29,11 +29,6 @@ namespace Tournament_Web_Interface.Controllers
             return View();
         }
 
-        public IActionResult ShowPlayers()
-        {
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
